d3963d656f1e6feddeb10564b50623db02f3bd80
eugeniaf <euggefran@gmail.com>
2016-11-12 20:40:30 -0300

    ejercicios

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -162,25 +162,71 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('NuevoEntrenamientoCtrl', function ($state, $scope, Ejercicios) {
-    $scope.listaEjercicios = false;
+  .controller('NuevoEntrenamientoCtrl', function ($state, $scope, $http, $localstorage) {
+    
+    $scope.mostrarListaEjercicios = false;
+    $scope.ejercicios = [];
+    
     console.log('NuevoEntrenamientoCtrl')
-    console.log($scope.listaEjercicios)
+    console.log($scope.mostrarListaEjercicios)
+    
     $scope.continuar = function () {
-      $scope.listaEjercicios = true;
-      console.log($scope.listaEjercicios)
+      /*
+      console.log('fecha continuar: ' + $scope.form.descripcion)
+      console.log('mensaje: ' + $scope.form.mensaje)
+      console.log('descripcion continuar: ' + $scope.form.fecha)*/
+      
+      $scope.mostrarListaEjercicios = true;
+      console.log($scope.mostrarListaEjercicios)
       
       var atletaId = 0;//$stateParams.atletaId;
-
       activar()
-      console.log(JSON.stringify(Ejercicios.all()))
-      // funcion que carga la lista de entrenamientos de un atleta
-      function activar() {
-        $scope.ejercicios = Ejercicios.all(); //Entrenamientos.get(/*atletaId*/0);
-      };      
       
-    }
+      // funcion que carga la lista de entrenamientos 
+      function activar() {
+      //  $scope.ejercicios = Ejercicios.all(); //Entrenamientos.get(/*atletaId*/0);
 
+            // se conecta a la API y obtiene la lista de ejercicios de un profesor
+            var token = $localstorage.get("access_token")
+            var fecha = '2016-10-03' //$scope.fecha
+            console.log('Fecha: ' + fecha)
+            $http.get('https://hectorapi.herokuapp.com/api/atleta/entrenamientos/' + fecha + '?token=' + token)
+              .success(function(data, status, headers,config){
+                  console.log('data success');
+                  console.log(data); // for browser console
+                  //if(resp.data){
+                    //$scope.ejercicios = resp.data[0].ejercicios;    
+                  //}              
+                  //console.log(' > ejercicios', $resp.data[0]);
+                })
+              .error(function(data, status, headers,config){
+                alert("Error al conectarse al servidor!")
+                console.log('data error '+ data);
+              })
+              .then(function(resp){
+console.log('no se que es esto '+ JSON.stringify(resp));
+              })
+              
+            /*
+            $http.get('https://hectorapi.herokuapp.com/api/profesor/entrenamientos/' + fecha + '?token=' + token)
+              .then(function(resp){
+                console.log('Success ejercicios', JSON.stringify(resp.data)); // JSON object 
+                //if(resp.data){
+                  //$scope.ejercicios = resp.data[0].ejercicios;    
+                //}              
+                console.log(' > ejercicios', $resp.data[0]);
+              }, function(err){
+                alert("Error al conectarse al servidor!")
+                console.error('ERR', err);
+              })*/      
+      };    
+      
+      // Agregar un ejercicio al string de ejercicios
+      function agregar(){
+          $scope.ejercicios.push('5')
+      } 
+       
+    }
   })
 
   .controller('EntrenamientoDetalleCtrl', function ($state, $stateParams, Ejercicios, $scope, $ionicModal) {
